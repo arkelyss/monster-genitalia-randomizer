dac4e748c9a1b8dddf0743b1dbe0ef5480b921dc
arkelyss <13072937+arkelyss@users.noreply.github.com>
2026-07-14 21:41:55 -0400

    chore(cleanup): Remove dead scripts and unused files

--- mgr/mods/mod_uninstaller.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtCore import QObject
-
-class ModUninstaller(QObject):
-    def __init__(self):
-        super().__init__()
-
-    def uninstall_mods(self, mod_names: list[str]):
-        for mod_name in mod_names:
-            
